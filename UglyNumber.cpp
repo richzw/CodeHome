0879e48891d64ec788565471a0bbd0aca00c0bc7
Wei Zang <richzw@gmail.com>
2013-04-09 13:55:13 +0800

    Update UglyNumber.cpp

--- UglyNumber.cpp
+++ UglyNumber.cpp
@@ -10,33 +10,31 @@ int Min(int a, int b, int c) {
 }
  
 int findNthUglyNum(int n) {
-  assert(n > 0);
+  	assert(n > 0);
 
-  int* pugly_number = new int[n+1];
+  	int* pugly_number = new int[n+1];
 	if (pugly_number == NULL)
 		return 0;
-
+	
 	pugly_number[0] = 1; 
 	int two = 0;
 	int three = 0;
 	int five = 0;
 	int last_index = 0;
 	int ugly_number = 1;
-
+	
 	while(last_index < n) {
 		// Get the min value of (2^x)*(3^y)*(5^z)
 		pugly_number[++last_index] = Min(pugly_number[two] * 2, pugly_number[three] * 3, pugly_number[five] * 5);
- 
-		if(pugly_number[two]*2 <= pugly_number[last_index]) two++;
- 
-		if(pugly_number[three]*3 <= pugly_number[last_index]) three++;
- 
+	
+		if(pugly_number[two]*2 <= pugly_number[last_index]) two++;	
+		if(pugly_number[three]*3 <= pugly_number[last_index]) three++;	
 		if(pugly_number[five]*5 <= pugly_number[last_index]) five++;
 	}
 	ugly_number = pugly_number[n-1];
 	copy(pugly_number, pugly_number+n, ostream_iterator<int>(cout, " "));
-
+	
 	delete[] pugly_number;
  
-  return ugly_number;
+  	return ugly_number;
 }
