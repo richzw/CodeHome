e0cb5e0f8877312fe65035cc003e8ee10c29abfd
Wei Zang <richzw@gmail.com>
2013-04-16 22:23:21 +0800

    Create findElementFromMatrix.cpp

---
+++ Algorithm/findElementFromMatrix.cpp
@@ -0,0 +1,7 @@
+/*
+ Q1: Given one matrix, each row are ascending ordered. each column is  ascending ordered.
+     To find one element in this matrix.
+     ex.      10  30 50
+              20  40 80,    find 40 in this matrix.
+     
+*/
