f92e09f4664749e2a939ce0057b4d3948e26257a
Wei Zang <richzw@gmail.com>
2013-04-17 22:19:42 +0800

    Update rebuildBTree.cpp

--- Algorithm/rebuildBTree.cpp
+++ Algorithm/rebuildBTree.cpp
@@ -2,3 +2,14 @@
  Q: rebuild the binary tree, according to preorder and inorder.
 
 */
+
+typedef struct node{
+  node* next;
+  int   data;
+ }node;
+
+node *build(const int *pre, const int *mid, const int n){
+ 
+ 
+ 
+}
