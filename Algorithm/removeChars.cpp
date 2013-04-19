658350a77206510703bb8112b2cdb35b1966ab55
Wei Zang <richzw@gmail.com>
2013-04-19 09:12:52 +0800

    Create removeChars.cpp

---
+++ Algorithm/removeChars.cpp
@@ -0,0 +1,9 @@
+/*
+ Q: Write an efficient function that deletes characters from an ASCII string. Use the prototype
+    		string removeChars( string str, string remove );
+    where any character existing in remove must be deleted from str. For example,
+	given a str of "Battle of the Vowels: Hawaii vs. Grozny" and a remove of
+	"aeiou", the function should transform str to “Bttl f th Vwls: Hw vs. Grzny”.
+*/
+
+
