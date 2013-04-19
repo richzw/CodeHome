ff11ef34feb1cd9604e103eabf10e2902aee1d01
Wei Zang <richzw@gmail.com>
2013-04-19 20:23:47 +0800

    Create findNONMin.cpp

---
+++ Algorithm/findNONMin.cpp
@@ -0,0 +1,5 @@
+/*
+   Q: Given a disorderly array of integers, how to find a value greater than 0, 
+      and not the smallest integer array. 
+      For example, [1,2,0] return [3,4, -1,1] returns 2. Preferably O (1) space, and O (n) time
+*/
