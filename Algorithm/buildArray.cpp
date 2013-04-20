943e6386b03f14272e3ae656825d354cd3d2dc99
Wei Zang <richzw@gmail.com>
2013-04-20 23:00:21 +0800

    Create buildArray.cpp

---
+++ Algorithm/buildArray.cpp
@@ -0,0 +1,8 @@
+/*
+ Q: Given array a [n],  Get array b [n] b [i] = a [0] * a [1] ... a [n-2] * a [n-1] / a [i]
+    Requirements can not be used to divide
+    In addition to traverse the counter with a [N] b [N], do not use the new variable 
+    (including stack temporary variables, space and global static variable)
+     Space complexity of O (1) time complexity of O (n)
+
+*/
