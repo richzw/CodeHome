b0195b3dba3e09b0e93acff468bba5c066f21a97
Wei Zang <richzw@gmail.com>
2013-04-24 11:19:34 +0800

    Create findMaxConsecutive.cpp

---
+++ Algorithm/findMaxConsecutive.cpp
@@ -0,0 +1,7 @@
+/*
+  Q: To an array of integers is found that contains the maximum number of consecutive subset, 
+     such as: 15, 7, 12, 6, 14, 13, 9, 11, returns: 5:[11, 12, 13, 14, 15]. 
+     The easiest method is to sort and then scan again, but the complexity is O(nlgn), 
+     Is there any O(n) method.
+*/
+
