fac996c1a20a1aa1f6c17601faeb95a945706342
Wei Zang <richzw@gmail.com>
2013-04-27 17:13:29 +0800

    Update IsConsecutiveArray.c

--- Algorithm/IsConsecutiveArray.c
+++ Algorithm/IsConsecutiveArray.c
@@ -2,14 +2,14 @@
   Q: Given an unsorted array of numbers, write a function that returns true if array consists of consecutive numbers.
 
 Examples:
-a) If array is {5, 2, 3, 1, 4}, then the function should return true because the array has consecutive numbers from 1 to 5.
-
-b) If array is {83, 78, 80, 81, 79, 82}, then the function should return true because the array has consecutive numbers from 78 to 83.
-
-c) If the array is {34, 23, 52, 12, 3 }, then the function should return false because the elements are not consecutive.
-
-d) If the array is {7, 6, 5, 5, 3, 4}, then the function should return false because 5 and 5 are not consecutive.
-
+a) If array is {5, 2, 3, 1, 4}, 
+   then the function should return true because the array has consecutive numbers from 1 to 5.
+b) If array is {83, 78, 80, 81, 79, 82}, 
+   then the function should return true because the array has consecutive numbers from 78 to 83.
+c) If the array is {34, 23, 52, 12, 3 }, 
+   then the function should return false because the elements are not consecutive.
+d) If the array is {7, 6, 5, 5, 3, 4}, 
+   then the function should return false because 5 and 5 are not consecutive.
 */
 
 Method 1 (Use Sorting)
