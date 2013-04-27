7f20c2736013e7bd3b60b5aab4733f7cc6d83261
Wei Zang <richzw@gmail.com>
2013-04-27 13:57:44 +0800

    Update and rename Sort.cpp? to Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -59,18 +59,43 @@ void merge(int arr[], int left, int mid, int right){
 			arr[k] = aux[i++];
 }
 
-void merge_no_aux(int arr[], int left, int mid, int right){
-
+void move(int* dest, int* src, int num){
+	for (int index = num-1; index >= 0; --index){
+		*(dest+index) = *(src+index);
+	}
 }
 
-void mergesort_BU(){
+void merge_no_aux(int arr[], int left, int mid, int right){
+	int first = left, last = mid + 1;
+	// One extra check, can we skip the merge?
+	if (arr[mid] < arr[last])
+		return;
+
+	while (first <= mid && last <= right){
+		if (arr[first] <= arr[last])
+			++first;
+		else{
+			int tmp = arr[last];
+			move(arr+first+1, arr+first, last-first);
+			arr[first] = tmp;
+			++first;
+			++mid;
+			++last;
+		}
+	}
+}
+
+/×
+× @func: merge sort from down to up method.
+×/
+void mergesort_BU(int arr[], int l, int r){
 	for (int m = 1; m < r; m = m+m)
 		for (int i = l; i <= r-m; i += m+m)
 			merge(arr, i, i+m-1, min(i+m+m-1, r));
 }
 
 /*
-* @func: merge sort implementation
+* @func: merge sort implementation, from up to down method.
 * @params: arr{array},
 *          left{int}, the left boundary of the array
 *          right{int}, the right boundary of the array
