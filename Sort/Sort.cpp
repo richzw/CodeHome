33d5dbcebe3258d31eae64a23d89aad6cafdb052
Wei Zang <richzw@gmail.com>
2013-05-09 09:42:14 +0800

    Update Sort.cpp

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -6,31 +6,6 @@
 
 
 
-// sort collections
- void insertSort(int a[], int len){
-     for (int j = 1; j < len; ++j){
-         int key = a[j];
-         int i = j - 1;
-         while(i >= 0 && a[i] > key){
-             a[i+1] = a[i];
-             i--;
-         }
-         a[i+1] = key;
-     }
- }
-
- void insertSort_recursive(int a[], int len){
-     if (len <= 1) return;
-     insertSort_recursive(a, len-1);
-     int key = a[len-1];
-     int i = len - 2;
-     while(i >= 0 && a[i] > key){
-         a[i+1] = a[i];
-         --i;
-     }
-     a[i+1] = key;
- }
-
 void shellsort(int arr[], int left, int right){
     int i, j, step;
 
