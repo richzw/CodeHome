d3d7e262fc0fa53b8bc908dd0c1502b4d7ffc2e5
Wei Zang <richzw@gmail.com>
2013-07-02 10:09:19 +0800

    Update findMaxConsecutive.cpp

--- Algorithm/findMaxConsecutive.cpp
+++ Algorithm/findMaxConsecutive.cpp
@@ -91,4 +91,56 @@ void union_set(int x, int y){
 	}
 }
 
-//here is hash map
+//here is another hash map
+/*
+用一个map<int, int>，它的key是一个起始的数字，value是这个起始数字起连续的个数。这样这个数组遍历一遍下来，
+只要map维护好了，自然就能得到最长的连续子串了，并且算法复杂度应该是O(n)。（不考虑map函数实现的复杂度）
+
+那么怎么来维护这个map呢？
+1取出当前的整数，在map里看一下是否已经存在，若存在则直接取下一个，不存在转2 (为什么要看是否已经存在，因为题目没有说不会有重复的数字。)
+2查看下map里面当前数字的前一个是否存在，如果存在，当前的最长长度就是前一个最长长度+1
+3查看下map里面当前数字的后一个是否存在，如果存在，那么就将以下一个数字开始的子串的最后一个更新下，
+    因为本来没有连上的2个子串，因为当前数字的出现连起来了
+4接着再看下前面数字是否存在，如果存在，就更新以这个数字结尾的子串的第一个数字的连续子串长度，原因同上
+*/
+     map<int, int> mapping;
+     int max = 0;
+     // The start point for the longest chain
+     int start = 0;
+ 
+     for (int i=0; i<size; i++) {
+         if (mapping.find(list[i]) == mapping.end()) {
+             int cur = list[i];
+             // Set current position as the start point for this potential longest chain
+             int cur_start = cur;
+             mapping.insert(make_pair(cur, 1));
+ 
+             map<int, int>::iterator prev = mapping.find(cur - 1);
+             map<int, int>::iterator next = mapping.find(cur + 1);
+ 
+             if (prev != mapping.end()) {
+                 // If previous number exists, increase current consecutive count
+                 mapping[cur] = prev->second + 1;
+             }
+ 
+             if (next != mapping.end()) {
+                 // Update the last one in the chain with the consecutive count from the one before current position
+                 int last = next->first + next->second - 1;
+                 mapping[last] = mapping[cur] = mapping[cur] + mapping[last];
+             }
+ 
+             if (prev != mapping.end()) {
+                 // Update the first one in the chain with the consecutive count from the one after current position
+                 int first = prev->first - prev->second + 1;
+                 mapping[first] = mapping[cur];
+ 
+                 // Use the first one as the start point for the whole chain
+                 cur_start = first;
+             }
+ 
+             if (mapping[cur_start] > max) {
+                 start = cur_start;
+                 max = mapping[cur_start];
+             }
+         }
+     }
