9e765c6fcd5d28af8780e00c5e0634f87a61f5ee
Wei Zang <richzw@gmail.com>
2013-07-02 17:25:58 +0800

    Update BST.c

--- DataStructure/BST.c
+++ DataStructure/BST.c
@@ -184,3 +184,27 @@ bool isBSTUtil(struct node* node, int min, int max){
    return isBSTUtil(node->left, min, node->data) &&
            isBSTUtil(node->right, node->data+1, max);
 }
+
+void printLevelOrder(struct node *root) {
+  if (!root) return;
+  queue<BinaryTree*> nodesQueue;
+  int nodesInCurrentLevel = 1;
+  int nodesInNextLevel = 0;
+  nodesQueue.push(root);
+  while (!nodesQueue.empty()) {
+    BinaryTree *currNode = nodesQueue.front();
+    nodesQueue.pop();
+    nodesInCurrentLevel--;
+    if (currNode) {
+      cout << currNode->data << " ";
+      nodesQueue.push(currNode->left);
+      nodesQueue.push(currNode->right);
+      nodesInNextLevel += 2;
+    }
+    if (nodesInCurrentLevel == 0) {
+      cout << endl;
+      nodesInCurrentLevel = nodesInNextLevel;
+      nodesInNextLevel = 0;
+    }
+  }
+}
