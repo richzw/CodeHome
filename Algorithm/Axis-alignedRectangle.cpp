0b15ad3ccc094ff253c3b57caac8b3ad8b896516
Wei Zang <richzw@gmail.com>
2013-07-25 09:22:21 +0800

    Create Axis-alignedRectangle.cpp

---
+++ Algorithm/Axis-alignedRectangle.cpp
@@ -0,0 +1,9 @@
+/*
+Describe an algorithm that takes an unsorted array of axis‐aligned rectangles and
+returns any pair of rectangles that overlaps, if there is such a pair. Axis‐aligned
+means that all the rectangle sides are either parallel or perpendicular to the x‐ and
+y‐axis. You can assume that each rectangle object has two variables in it: the x‐y
+coordinates of the upper‐left corner and the bottom‐right corner.
+*/
+
+
