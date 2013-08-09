f4b8e43292840323794f03ad1e2c6f554ccc4410
Wei Zang <richzw@gmail.com>
2013-08-09 09:35:08 +0800

    Update UglyNumber.cpp

--- LeetCode/UglyNumber.cpp
+++ LeetCode/UglyNumber.cpp
@@ -1,3 +1,40 @@
+/*This function divides a by greatest divisible 
+  power of b*/
+int maxDivide(int a, int b)
+{
+  while (a%b == 0)
+   a = a/b; 
+  return a;
+}   
+ 
+/* Function to check if a number is ugly or not */
+int isUgly(int no)
+{
+  no = maxDivide(no, 2);
+  no = maxDivide(no, 3);
+  no = maxDivide(no, 5);
+   
+  return (no == 1)? 1 : 0;
+}    
+ 
+/* Function to get the nth ugly number*/
+int getNthUglyNo(int n)
+{
+  int i = 1; 
+  int count = 1;   /* ugly number count */
+ 
+  /*Check for all integers untill ugly count 
+    becomes n*/
+  while (n > count)
+  {
+    i++;      
+    if (isUgly(i))
+      count++; 
+  }
+  return i;
+}
+
+
 /*
 Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 
 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, ... 
