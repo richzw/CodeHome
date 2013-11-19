373b6b523573e19284a7067096cfbf2fc8ccd3fb
Wei Zang <richzw@gmail.com>
2013-11-19 10:18:14 +0800

    Update qsort.py

--- Python/qsort.py
+++ Python/qsort.py
@@ -1,3 +1,5 @@
-
 def qsort(l): 
-     return l if len(l)<=1 else qsort([x for x in l[1:] if x<l[0]]) + [l[0]] + qsort([x for x in l[1:] if x>=l[0]])
+     if len(l) <= 1:
+          return l
+     else:
+          qsort([x for x in l[1:] if x<l[0]]) + [l[0]] + qsort([x for x in l[1:] if x>=l[0]])
