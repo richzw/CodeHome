aee69ecc84edcc6ce84cfcf84d4497257d7a84fa
Wei Zang <richzw@gmail.com>
2013-12-25 09:17:46 +0800

    Update ListToBST.c

--- Algorithm/ListToBST.c
+++ Algorithm/ListToBST.c
@@ -1,5 +1,5 @@
 /*
-Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+Q1: Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
 
 top-down solution: run time complexity is O(NlgN), easy to implementation
 bottom-up solution: run time complexity is O(N), difficult to verify
@@ -31,20 +31,14 @@ bst_node* sortedlistToBST(list_node* sortList, int len){
 }
 
 /*
-Convert a BST to a sorted circular doubly-linked list in-place. 
+Q2: Convert a BST to a sorted circular doubly-linked list in-place. 
 Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
 http://cslibrary.stanford.edu/109/TreeListRecursion.html
-*/
-/*
- helper function -- given two list nodes, join them
- together so the second immediately follow the first.
- Sets the .next of the first and the .previous of the second.
 */
 static void join(Node a, Node b) {
     a->large = b;
     b->small = a;
 }
-
 
 /*
  helper function -- given two circular doubly linked
@@ -90,3 +84,39 @@ static Node treeToList(Node root) {
     
     return(aList);
 }
+
+// another solution
+typedef struct double_list_node{
+	int data;
+	double_list_node* prev;
+	double_list_node* next;
+}double_list_node;
+
+void bstToListHelper(bst_node* tree, double_list_node*& prevnode, double_list_node*& head){
+	if (!tree)
+		return ;
+
+	bstToListHelper(tree->left, prevnode, head);
+	tree->left = (bst_node*)prevnode;
+	if (prevnode)
+		prevnode->prev = (double_list_node*)tree;
+	else
+		head = (double_list_node*)tree;
+
+	// The head always point to the tail 
+	double_list_node* right = (double_list_node*)tree->right;
+	head->prev = (double_list_node*)tree;
+	((double_list_node*)tree)->next = head;
+
+	prevnode = (double_list_node*)tree;
+	bstToListHelper((bst_node*)right, prevnode, head);
+}
+
+double_list_node* bstToList(bst_node* tree){
+	double_list_node* prev = NULL;
+	double_list_node* head = NULL;
+	bstToListHelper(tree, prev, head);
+
+	return head;
+}
+
