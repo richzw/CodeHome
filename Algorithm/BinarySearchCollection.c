7992ea7dcab00f0d65fb2ee8c5673060a138bf19
Wei Zang <richzw@gmail.com>
2014-03-05 09:27:03 +0800

    Update BinarySearchCollection.c

--- Algorithm/BinarySearchCollection.c
+++ Algorithm/BinarySearchCollection.c
@@ -127,3 +127,31 @@ int findKthFromTwoArrays(int A[], int m, int B[], int n, int k){
 		return findKthFromTwoArrays(B, n, A, m, k);
 	}
 }
+
+/*
+一个有序（升序）数组，没有重复元素，在某一个位置发生了旋转后，求target在变化后的数组中出现的位置，不存在则返回-1
+*/
+int searchInRotateArr(int A[], int n, int target){
+	int low = 0, high = n - 1;
+
+	while (low <= high){
+		int mid = low + (high - low)>>1;
+		if (A[mid] == target)
+			return mid;
+		else if (A[mid] > target){
+			// low ... mid is ascending order
+			if (target >= A[low] && target < A[mid])
+				high = mid - 1;
+			else
+				low = mid + 1;
+		}else {
+			// mid ... high is ascending order
+			if (target >= A[mid] && target < A[high])
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+	}
+
+	return -1;
+}
