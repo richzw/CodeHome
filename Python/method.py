fbc6f0458c30e6e1de99da2e691aa8e2f194fb29
Wei Zang <richzw@gmail.com>
2014-03-20 14:28:12 +0800

    Update method.py

--- Python/method.py
+++ Python/method.py
@@ -3,3 +3,11 @@ class Human(object):
         self.weight = weight
     def get_weight(self):
         return self.weight
+
+
+In [10]: Human.get_weight(Human(45))
+Out[10]: 45
+
+In [11]: person = Human(45)
+In [12]: person.get_weight()
+Out[12]: 45
