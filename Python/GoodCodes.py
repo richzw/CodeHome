117d10b06e6e04e6e342d724ec74b051b18c40e0
Wei Zang <richzw@gmail.com>
2014-03-25 09:22:45 +0800

    Update GoodCodes.py

--- Python/GoodCodes.py
+++ Python/GoodCodes.py
@@ -25,16 +25,49 @@ set([1, 3, 4, 5, 6, 7])
 >>> sort_aa  
 [('a', '1'), ('sss', '2'), ('ffff2', '3'), ('ffdf', '5')] 
 
-6
+6 #convert dictionary to string
 >>> params = {"server":"mpilgrim", "database":"master", "uid":"sa", "pwd":"secret"}  
 >>> ["%s=%s" % (k, v) for k, v in params.items()]  
 ['server=mpilgrim', 'uid=sa', 'database=master', 'pwd=secret']  
 >>> ";".join(["%s=%s" % (k, v) for k, v in params.items()])  
 'server=mpilgrim;uid=sa;database=master;pwd=secret' 
-
+#convert string to dictionary
 >>> a = 'server=mpilgrim;uid=sa;database=master;pwd=secret' 
 >>> aa = {}  
 >>> for i in a.split(';'):aa[i.split('=',1)[0]] = i.split('=',1)[1]  
 ...   
 >>> aa  
 {'pwd': 'secret', 'database': 'master', 'uid': 'sa', 'server': 'mpilgrim'} 
+
+#将时间对象转换成字符串
+>>> import datetime
+>>> datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+  '2011-01-20 14:05'
+ 
+#时间大小比较
+>>> import time
+>>> t1 = time.strptime('2011-01-20 14:05',"%Y-%m-%d %H:%M")
+>>> t2 = time.strptime('2011-01-20 16:05',"%Y-%m-%d %H:%M")
+>>> t1 > t2
+  False
+>>> t1 < t2
+  True
+ 
+#时间差值计算,计算8小时前的时间
+>>> datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+  '2011-01-20 15:02'
+>>> (datetime.datetime.now() - datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M")
+  '2011-01-20 07:03'
+ 
+#将字符串转换成时间对象
+>>> endtime=datetime.datetime.strptime('20100701',"%Y%m%d")
+>>> type(endtime)
+  <type 'datetime.datetime'>
+>>> print endtime
+  2010-07-01 00:00:00
+ 
+#将从 1970-01-01 00:00:00 UTC 到现在的秒数，格式化输出   
+>>> import time
+>>> a = 1302153828
+>>> time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(a))
+  '2011-04-07 13:23:48'
