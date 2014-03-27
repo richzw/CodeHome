2bfc991354b029df1e01bc284a5930bce4e61920
Wei Zang <richzw@gmail.com>
2014-03-27 14:57:55 +0800

    Create Decorator.py

---
+++ Python/Decorator.py
@@ -0,0 +1,13 @@
+def hello(fn):
+    def wrapper():
+        print "hello, %s" % fn.__name__
+        fn()
+        print "goodby, %s" % fn.__name__
+    return wrapper
+ 
+@hello
+def foo():
+    print "i am foo"
+ 
+foo()
+
