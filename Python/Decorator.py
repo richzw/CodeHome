729d80d3a2e1e2ad38d374bbc86d4181963113de
Wei Zang <richzw@gmail.com>
2014-03-28 15:01:00 +0800

    Update Decorator.py

--- Python/Decorator.py
+++ Python/Decorator.py
@@ -11,3 +11,19 @@ def foo():
  
 foo()
 
+#----------------------------------------------------------------
+def makeHtmlTag(tag, *args, **kwds):
+    def real_decorator(fn):
+        css_class = " class='{0}'".format(kwds["css_class"]) \
+                                     if "css_class" in kwds else ""
+        def wrapped(*args, **kwds):
+            return "<"+tag+css_class+">" + fn(*args, **kwds) + "</"+tag+">"
+        return wrapped
+    return real_decorator
+ 
+@makeHtmlTag(tag="b", css_class="bold_css")
+@makeHtmlTag(tag="i", css_class="italic_css")
+def hello():
+    return "hello world"
+ 
+print hello()
