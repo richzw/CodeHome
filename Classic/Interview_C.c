9854043605e4229cb36dd5a2670212434b526401
Wei Zang <richzw@gmail.com>
2014-04-01 22:32:49 +0800

    Update Interview_C.c

--- Classic/Interview_C.c
+++ Classic/Interview_C.c
@@ -18,3 +18,21 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 	
 }
+
+
+#include <stdlib.h>
+#include <string.h>
+ 
+struct line {
+   int length;
+   char contents[0]; // C99的玩法是：char contents[]; 没有指定数组长度
+};
+ 
+int main(){
+    int this_length=10;
+    struct line *thisline = (struct line *)
+                     malloc (sizeof (struct line) + this_length);
+    thisline->length = this_length;
+    memset(thisline->contents, 'a', this_length);
+    return 0;
+}
