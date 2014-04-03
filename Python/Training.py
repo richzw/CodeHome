3df20ffb580464dc0bbb98164a6bd6e9c76bcf7f
Wei Zang <richzw@gmail.com>
2014-04-03 16:55:43 +0800

    Update Training.py

--- Python/Training.py
+++ Python/Training.py
@@ -146,5 +146,41 @@ def hello():
 print hello()
 
 
+#class decorator
+class myDecorator(object):
+    def __init__(self, fn):
+        print "inside myDecorator.__init__()"
+        self.fn = fn
+    def __call__(self):
+        self.fn()
+        print "inside myDecorator.__call__()"
+@myDecorator
+def aFunction():
+    print "inside aFunction()"
+
+print "Finished decorating aFunction()"
+
+aFunction()
+
+class makeHtmlTag(object):
+    def __init__(self, tag, css_class=""):
+        self._tag = tag
+        self._css_class = " class = '{0}'".format(css_class)\
+                          if css_class != "" else ""
+    def __call__(self, fn):
+        def wrapped(*args, **kwargs):
+            return "<" + self._tag +self._css_class +">"\
+                   + fn(*args, **kwargs) + "</" + self._tag + ">"
+        return wrapped
+    
+@makeHtmlTagClass(tag="b", css_class="bold_css")
+@makeHtmlTagClass(tag="i", css_class="italic_css")
+def hello(name):
+    return "Hello, {}".format(name)
+ 
+print hello("Benz")
+
+
+
 if __name__ == '__main__':
     pass
