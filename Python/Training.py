c4e51d083b990e54b494fbaf62d7a0acaed5e9e4
Wei Zang <richzw@gmail.com>
2014-04-04 09:27:34 +0800

    Update Training.py

--- Python/Training.py
+++ Python/Training.py
@@ -328,3 +328,59 @@ print sum_num(1000000)
 
 if __name__ == '__main__':
     pass
+
+'''
+from inspect import getmembers, getargspec
+from functools import wraps
+ 
+def wraps_decorator(f):
+    @wraps(f)
+    def wraps_wrapper(*args, **kwargs):
+        return f(*args, **kwargs)
+    return wraps_wrapper
+ 
+class SomeClass(object):
+    @wraps_decorator
+    def method(self, x, y):
+        pass
+ 
+obj = SomeClass()
+for name, func in getmembers(obj, predicate=inspect.ismethod):
+    print "Member Name: %s" % name
+    print "Func Name: %s" % func.func_name
+    print "Args: %s" % getargspec(func)[0]
+ 
+# Member Name: method
+# Func Name: method
+# Args: []
+
+def get_true_argspec(method):
+    argspec = inspect.getargspec(method)
+    args = argspec[0]
+    if args and args[0] == 'self':
+        return argspec
+    if hasattr(method, '__func__'):
+        method = method.__func__
+    if not hasattr(method, 'func_closure') or method.func_closure is None:
+        raise Exception("No closure for method.")
+ 
+    method = method.func_closure[0].cell_contents
+    return get_true_argspec(method)
+
+import cProfile, pstats, StringIO
+ 
+def profiler(func):
+    def wrapper(*args, **kwargs):
+        datafn = func.__name__ + ".profile" # Name the data file
+        prof = cProfile.Profile()
+        retval = prof.runcall(func, *args, **kwargs)
+        #prof.dump_stats(datafn)
+        s = StringIO.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(prof, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print s.getvalue()
+        return retval
+ 
+    return wrapper    
+'''
