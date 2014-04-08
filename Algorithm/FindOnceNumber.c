faf5ac530da881cc1fca16252d60873fb59902d6
Wei Zang <richzw@gmail.com>
2014-04-08 20:54:01 +0800

    Update FindOnceNumber.c

--- Algorithm/FindOnceNumber.c
+++ Algorithm/FindOnceNumber.c
@@ -92,3 +92,48 @@ int singleNumber(int A[], int n) {
     return ones;
 }
 
+//更好的方法，而且可以扩展到多位计算的方法。
+int getSingleNumber_v2(int a[], int len){
+	if (a == NULL)
+		return 0;
+	int x0 = ~0, x1 = 0, x2 = 0, tmp;
+	for (int i = 0; i < len; ++i){
+		tmp = x2;
+		x2 = (x1 & a[i]) | (x2 & ~a[i]);
+        x1 = (x0 & a[i]) | (x1 & ~a[i]);
+        x0 = (tmp & a[i]) | (x0 & ~a[i]);
+	}
+	return x1;
+}
+//利用上述方法可以解决这个扩展问题：
+//Q:Given an array of integers, every element appears k times except for one. Find that single one who appears l times.
+int getSingleNumber_v3(int A[], int len, int k, int l){
+    if (A == NULL) return 0;
+    int t;
+    int* x = new int[k];
+	memset(x, 0, k*sizeof(int));
+    x[0] = ~0;
+
+    for (int i = 0; i < len; i++) {
+        t = x[k-1];
+        for (int j = k-1; j > 0; j--) {
+            x[j] = (x[j-1] & A[i]) | (x[j] & ~A[i]);
+        }
+        x[0] = (t & A[i]) | (x[0] & ~A[i]);
+    }
+    return x[l];
+}
+
+
+/*
+扩展一：
+给定一个包含n个整数的数组，除了一个数出现二次外所有的整数均出现三次，找出这个只出现二次的整数。ones记录1出现一次的数，
+twos记录1出现2次的数，容易知道twos记录的即是最终结果。
+扩展二：
+给定一个包含n个整数的数组，有一个整数x出现b次，一个整数y出现c次，其他所有的数均出现a次，其中b和c均不是a的倍数，找出x和y。
+使用二 进制模拟a进制，累计二进制位1出现的次数，当次数达到a时，对其清零，这样可以得到b mod a次x，c mod a次y的累加。
+遍历剩余结果（用ones、twos、fours...变量表示）中每一位二进制位1出现的次数，如果次数为b mod a 或者 c mod a，
+可以说明x和y的当前二进制位不同（一个为0，另一个为1），据此二进制位将原数组分成两组， 一组该二进制位为1，
+另一组该二进制位为0。这样问题变成“除了一个整数出现a1次（a1 = b 或 a1 = c）外所有的整数均出现a次”，
+使用和上面相同的方式计算就可以得到最终结果，假设模拟a进制计算过程中使用的变量为ones、twos、 fours...那么最终结果可以用ones | twos | fours ...表示。
+*/
