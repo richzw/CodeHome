65bc01316ba18d3725699a382ac26941a036506b
Wei Zang <richzw@gmail.com>
2014-04-13 00:03:43 +0800

    Create PassByObject.py

---
+++ Python/PassByObject.py
@@ -0,0 +1,35 @@
+
+# list - mutable variable
+def try_to_change_list_contents(the_list):
+    print 'got', the_list
+    the_list.append('four')
+    print 'changed to', the_list
+
+outer_list = ['one', 'two', 'three']
+
+print 'before, outer_list =', outer_list
+try_to_change_list_contents(outer_list)
+print 'after, outer_list =', outer_list
+
+def try_to_change_list_reference(the_list):
+    print 'got', the_list
+    the_list = ['and', 'we', 'can', 'not', 'lie']
+    print 'set to', the_list
+
+outer_list = ['we', 'like', 'proper', 'English']
+
+print 'before, outer_list =', outer_list
+try_to_change_list_reference(outer_list)
+print 'after, outer_list =', outer_list
+
+# string - immutable variable
+def try_to_change_string_reference(the_string):
+    print 'got', the_string
+    the_string = 'In a kingdom by the sea'
+    print 'set to', the_string
+
+outer_string = 'It was many and many a year ago'
+
+print 'before, outer_string =', outer_string
+try_to_change_string_reference(outer_string)
+print 'after, outer_string =', outer_string
