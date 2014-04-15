7eb991624395fa1ac32c43321f7d5e86baf06fa5
Wei Zang <richzw@gmail.com>
2014-04-15 13:43:51 +0800

    Update PairOfSum.py

--- Python/PairOfSum.py
+++ Python/PairOfSum.py
@@ -1,5 +1,22 @@
-#
+#Given an integer array, output all pairs that sum up to a specific value k. 
 
+# O(NlogN)
+def mySum(arr, ss):
+	if len(arr) < 2:
+		return
+	arr.sort()
+	beginIdx, endIdx = (0, len(arr)-1)
+	while beginIdx < endIdx:
+		curSum = arr[beginIdx] + arr[endIdx]
+		if curSum == ss:
+			print arr[beginIdx], arr[endIdx]
+			beginIdx += 1
+		elif curSum < ss:
+			beginIdx += 1
+		else:
+			endIdx -= 1
+
+# O(N)
 def pairOfSum(arr, ss):
 	if len(arr) < 2:
 		return
