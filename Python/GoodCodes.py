3b22951330a8e74a22aad31089c3bc6b94debd39
Wei Zang <richzw@gmail.com>
2014-04-16 10:23:15 +0800

    Update GoodCodes.py

--- Python/GoodCodes.py
+++ Python/GoodCodes.py
@@ -71,3 +71,33 @@ set([1, 3, 4, 5, 6, 7])
 >>> a = 1302153828
 >>> time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(a))
   '2011-04-07 13:23:48'
+  
+#Bad
+# create a concatenated string from 0 to 19 (e.g. "012..1819")
+nums = ""
+for n in range(20):
+  nums += str(n)   # slow and inefficient
+print nums
+#Good
+# create a concatenated string from 0 to 19 (e.g. "012..1819")
+nums = []
+for n in range(20):
+  nums.append(str(n))
+print "".join(nums)  # much more efficient
+#Best
+# create a concatenated string from 0 to 19 (e.g. "012..1819")
+nums = [str(n) for n in range(20)]
+print "".join(nums)
+
+foo = 'foo'
+bar = 'bar'
+foobar = foo + bar  # This is good
+foo += 'ooo'  # This is bad, instead you should do:
+foo = ''.join([foo, 'ooo'])
+
+foo = 'foo'
+bar = 'bar'
+foobar = '%s%s' % (foo, bar) # It is OK
+foobar = '{0}{1}'.format(foo, bar) # It is better
+foobar = '{foo}{bar}'.format(foo=foo, bar=bar) # It is best
+  
