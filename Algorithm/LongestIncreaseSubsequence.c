f7b1648f970d2d94e29b6712e2eb51992aa1d7e7
Wei Zang <richzw@gmail.com>
2014-04-23 13:27:38 +0800

    Update LongestIncreaseSubsequence.c

--- Algorithm/LongestIncreaseSubsequence.c
+++ Algorithm/LongestIncreaseSubsequence.c
@@ -1,3 +1,9 @@
+/*
+The longest Increasing Subsequence (LIS) problem is to find the length of the longest subsequence of a given sequence 
+such that all elements of the subsequence are sorted in increasing order. 
+For example, length of LIS for { 10, 22, 9, 33, 21, 50, 41, 60, 80 } is 6 and LIS is {10, 22, 33, 50, 60, 80}.
+*/
+
 //Solution1: dp[i]=max(dp[j])+1,(j∈[1, i-1])
 int LIS(int arr[1000], int n)
 {
