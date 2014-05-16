c7c7d8051dfb30935dd092d230199ef8ca75146a
Wei Zang <richzw@gmail.com>
2014-05-16 13:52:27 +0800

    Create ZorderCurve.c

---
+++ DataStructure/ZorderCurve.c
@@ -0,0 +1,23 @@
+//How could i access data which is being stored using Z-order with O(1) time complexity in array?
+
+uint32_t calcZOrder(uint16_t xPos, uint16_t yPos)
+{
+    static const uint32_t MASKS[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
+    static const uint32_t SHIFTS[] = {1, 2, 4, 8};
+
+    uint32_t x = xPos;  // Interleave lower 16 bits of x and y, so the bits of x
+    uint32_t y = yPos;  // are in the even positions and bits from y in the odd;
+
+    x = (x | (x << SHIFTS[3])) & MASKS[3];
+    x = (x | (x << SHIFTS[2])) & MASKS[2];
+    x = (x | (x << SHIFTS[1])) & MASKS[1];
+    x = (x | (x << SHIFTS[0])) & MASKS[0];
+
+    y = (y | (y << SHIFTS[3])) & MASKS[3];
+    y = (y | (y << SHIFTS[2])) & MASKS[2];
+    y = (y | (y << SHIFTS[1])) & MASKS[1];
+    y = (y | (y << SHIFTS[0])) & MASKS[0];
+
+    const uint32_t result = x | (y << 1);
+    return result;
+}
