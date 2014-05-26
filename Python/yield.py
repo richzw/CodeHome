8d1c55d50f204247b4c263e50d07890a35fc524b
Wei Zang <richzw@gmail.com>
2014-05-26 14:13:39 +0800

    Update yield.py

--- Python/yield.py
+++ Python/yield.py
@@ -1,5 +1,55 @@
+"""
+To understand what yield does, you must understand what generators are. And before generators come iterables.
+
+Iterable:
+>>> mylist = [x*x for x in range(3)]
+>>> for i in mylist:
+...    print(i)
+0
+1
+4
+Everything you can use "for... in..." on is an iterable: lists, strings, files... These iterables are handy 
+because you can read them as much as you wish, but you store all the values in memory and it's not always 
+what you want when you have a lot of values.
+
+Generator:
+Generators are iterators, but you can only iterate over them once. 
+It's because they do not store all the values in memory, they generate the values on the fly:
+>>> mygenerator = (x*x for x in range(3))
+>>> for i in mygenerator:
+...    print(i)
+0
+1
+4
+
+Yield:
+Yield is a keyword that is used like return, except the function will return a generator.
+>>> def createGenerator():
+...    mylist = range(3)
+...    for i in mylist:
+...        yield i*i
+...
+>>> mygenerator = createGenerator() # create a generator
+>>> print(mygenerator) # mygenerator is an object!
+<generator object createGenerator at 0xb7555c34>
+>>> for i in mygenerator:
+...     print(i)
+0
+1
+4
+To master yield, you must understand that when you call the function, 
+the code you have written in the function body does not run. The function only returns the generator object, 
+
+The first time the for calls the generator object created from your function, 
+it will run the code in your function from the beginning until it hits yield, 
+then it'll return the first value of the loop. Then, each other call will run the loop you have written in the function 
+one more time, and return the next value, until there is no value to return.
+
+"""
+
+
+
 # tree tranverse 
-   
  # wrong version
  def traverse_wrong(node):          
      if isinstance(node, list):     
