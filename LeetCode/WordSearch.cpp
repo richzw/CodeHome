88a4960394f5c20615f44240f771d0a10b530a1d
Wei Zang <richzw@gmail.com>
2014-06-09 21:54:14 +0800

    Update WordSearch.cpp

--- LeetCode/WordSearch.cpp
+++ LeetCode/WordSearch.cpp
@@ -32,4 +32,40 @@ Marking a node as visited can be done many ways. A nice trick is to use the most
 if the node has been visited or not.
 */
 
+class Solution:
+    # @param board, a list of lists of 1 length string
+    # @param word, a string
+    # @return a boolean
+    def exist(self, board, word):
+        self.totalRow, self.totalCol = len(board), len(board[0])
+        for i in xrange(self.totalRow):
+            for j in xrange(self.totalCol):
+                if board[i][j] == word[0]:
+                    if self.dfs(board, i, j, word[1:]): return True
+        return False
+         
+    def dfs(self, board, r, c, word):
+        if len(word) == 0: return True
+        # Up
+        if (r > 0 and board[r - 1][c] == word[0]):
+            ch, board[r][c] = board[r][c], '#'
+            if self.dfs(board, r - 1, c, word[1:]): return True
+            board[r][c] = ch
+        # Down
+        if (r < self.totalRow - 1 and board[r + 1][c] == word[0]):
+            ch, board[r][c] = board[r][c], '#'
+            if self.dfs(board, r + 1, c, word[1:]): return True
+            board[r][c] = ch
+        # Left
+        if (c > 0 and board[r][c - 1] == word[0]):
+            ch, board[r][c] = board[r][c], '#'
+            if self.dfs(board, r, c - 1, word[1:]): return True
+            board[r][c] = ch    
+        # Right
+        if (c < self.totalCol - 1 and board[r][c + 1] == word[0]):
+            ch, board[r][c] = board[r][c], '#'
+            if self.dfs(board, r, c + 1, word[1:]): return True
+            board[r][c] = ch    
+        return False
+
 
