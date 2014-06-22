b1bee675581c99dbfc44a58d51155bc831a4a273
Wei Zang <richzw@gmail.com>
2014-06-22 21:05:52 +0800

    Create UnionBySize.c

---
+++ Algorithm/UnionFind/UnionBySize.c
@@ -0,0 +1,9 @@
+/*
+Develop a union-find implementation that uses the same basic strategy as weighted quick-union 
+but keeps track of tree height and always links the shorter tree to the taller one. 
+Prove a lgN upper bound on the height of the trees for N sites with your algorithm.
+*/
+
+//hint: replace the sz[] array with a ht[] array such that ht[i] stores the height of the subtree rooted at i
+
+
