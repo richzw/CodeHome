5e47e820ba369f56bfaff710093d8525a7736aa0
Wei Zang <richzw@gmail.com>
2014-06-23 22:25:15 +0800

    Create UnioinByHeight.c

---
+++ Algorithm/UnionFind/UnioinByHeight.c
@@ -0,0 +1,6 @@
+/*
+ The idea is to always attach smaller depth tree under the root of the deeper tree. This technique is called union by rank. 
+ The term rank is preferred instead of height because if path compression technique (we have discussed it below) is used, 
+ then rank is not always equal to height. Also, size (in place of height) of trees can also be used as rank.
+ 
+*/
