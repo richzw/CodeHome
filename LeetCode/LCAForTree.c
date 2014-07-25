f1ce9297f97029498afe2bcd6b2ea32bedd0e604
Wei Zang <richzw@gmail.com>
2014-07-25 09:03:48 +0800

    Update LCAForTree.c

--- LeetCode/LCAForTree.c
+++ LeetCode/LCAForTree.c
@@ -65,6 +65,68 @@ Node *LCA(Node *root, Node *p, Node *q) {
 	  	return root;  // if p and q are on both sides
 	return L ? L : R;  // either one of p,q is on one side OR p,q is not in L&R subtrees
 }
+TreeNode *findLCAUtil(TreeNode* root, int n1, int n2, bool &v1, bool &v2)
+{
+    // Base case
+    if (root == NULL) return NULL;
+ 
+    // If either n1 or n2 matches with root's key, report the presence
+    // by setting v1 or v2 as true and return root (Note that if a key
+    // is ancestor of other, then the ancestor key becomes LCA)
+    if (root->data == n1)
+    {
+        v1 = true;
+        return root;
+    }
+    if (root->data == n2)
+    {
+        v2 = true;
+        return root;
+    }
+ 
+    // Look for keys in left and right subtrees
+    TreeNode *left_lca  = findLCAUtil(root->left, n1, n2, v1, v2);
+    TreeNode *right_lca = findLCAUtil(root->right, n1, n2, v1, v2);
+ 
+    // If both of the above calls return Non-NULL, then one key
+    // is present in once subtree and other is present in other,
+    // So this node is the LCA
+    if (left_lca && right_lca)  return root;
+ 
+    // Otherwise check if left subtree or right subtree is LCA
+    return (left_lca != NULL)? left_lca: right_lca;
+}
+ 
+// Returns true if key k is present in tree rooted with root
+bool find(TreeNode *root, int k)
+{
+    // Base Case
+    if (root == NULL)
+        return false;
+ 
+	if (root->data == k || find(root->left, k) ||  find(root->right, k))
+        return true;
+
+    return false;
+}
+ 
+// This function returns LCA of n1 and n2 only if both n1 and n2 are present
+// in tree, otherwise returns NULL;
+TreeNode *findLCA(TreeNode *root, int n1, int n2)
+{
+    // Initialize n1 and n2 as not visited
+    bool v1 = false, v2 = false;
+ 
+    // Find lca of n1 and n2 using the technique discussed above
+    TreeNode *lca = findLCAUtil(root, n1, n2, v1, v2);
+ 
+    // Return LCA only if both n1 and n2 are present in tree
+    if (v1 && v2 || v1 && find(lca, n2) || v2 && find(lca, n1))
+        return lca;
+ 
+    return NULL;
+}
+
 
 /*
 * Q3: Given a binary tree, find the lowest common ancestor of two given nodes in the tree. 
