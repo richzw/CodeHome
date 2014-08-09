251457a26fd9728f738c60ae88d77e24f5084e70
Wei Zang <richzw@gmail.com>
2014-08-09 22:41:32 +0800

    Update BuildTreeInorderLevel.c

--- DataStructure/BuildTreeInorderLevel.c
+++ DataStructure/BuildTreeInorderLevel.c
@@ -1,1 +1,8 @@
 //Construct a tree from Inorder and Level order traversals
+/*
+Input: Two arrays that represent Inorder
+       and level order traversals of a 
+       Binary Tree
+in[]    = {4, 8, 10, 12, 14, 20, 22};
+level[] = {20, 8, 22, 4, 12, 10, 14};
+*/
