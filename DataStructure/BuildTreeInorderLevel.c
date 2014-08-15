da2521b70a2c041d49e7000a64400d62761c4c44
Wei Zang <richzw@gmail.com>
2014-08-15 12:15:44 +0800

    Update BuildTreeInorderLevel.c

--- DataStructure/BuildTreeInorderLevel.c
+++ DataStructure/BuildTreeInorderLevel.c
@@ -7,9 +7,16 @@ in[]    = {4, 8, 10, 12, 14, 20, 22};
 level[] = {20, 8, 22, 4, 12, 10, 14};
 */
 
-int getIndex(){
+int getIndex(int target, int in[], int start, int stop){
+       if (in == NULL)
+              return 0;
        
+       for (int index = start; index < stop; ++index){
+              
+              
+       }
        
+       return -1;
 }
 
 int* getElements(){
@@ -23,7 +30,7 @@ TreeNode* buildTreeByInorderLevel(int in[], int level[], int start, int stop){
        
        TreeNode* node = new TreeNode(level[start]);
        
-       int index = getIndex(in, start, end);
+       int index = getIndex(node->data, in, start, stop);
        
        int* pLeft = getElements();
        int* pRight = getElements();
