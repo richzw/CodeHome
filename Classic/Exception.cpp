c1447edf24defa416d0d18caa3acf4bd2ab5dc15
Wei Zang <richzw@gmail.com>
2014-09-16 09:25:56 +0800

    Update Exception.cpp

--- Classic/Exception.cpp
+++ Classic/Exception.cpp
@@ -2,26 +2,26 @@
 //http://stackoverflow.com/questions/541235/how-to-build-a-c-dll-wrapper-that-catches-all-exceptions
 //http://blogs.msdn.com/b/jaredpar/archive/2008/01/11/mixing-seh-and-c-exceptions.aspx
 
-      try{
-      				//…
-			}catch(std::bad_alloc& e)
-			{
-				cout << “Catch bad alloc exception ” << e.what() << endl;
-			}
-			catch(std::bad_cast& e)
-			{
-				cout << “Catch bad alloc exception ” << e.what() << endl;
-			}
-			catch(std::bad_exception& e)
-			{
-        cout << “Catch bad alloc exception ” << e.what() << endl;
-			}
-			// catch more exception types here
-			// … 
-			catch(...)
-			{
-				// how to get the content of unkown exception?
-			}
+try{
+     //…
+}catch(std::bad_alloc& e)
+{
+	cout << “Catch bad alloc exception ” << e.what() << endl;
+}
+catch(std::bad_cast& e)
+{
+	cout << “Catch bad alloc exception ” << e.what() << endl;
+}
+catch(std::bad_exception& e)
+{
+	cout << “Catch bad alloc exception ” << e.what() << endl;
+}
+// catch more exception types here
+// … 
+catch(...)
+{
+	// how to get the content of unkown exception?
+}
 
 
 #include <iostream>
