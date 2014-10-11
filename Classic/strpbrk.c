7ae30f936349d1cf5ac85406e9b3c71560e6a688
Wei Zang <richzw@gmail.com>
2014-10-11 10:10:13 +0800

    Update strpbrk.c

--- Classic/strpbrk.c
+++ Classic/strpbrk.c
@@ -6,3 +6,21 @@ char *strpbrk(const char *s1, const char *s2)
             return (char*)--s1;
     return 0;
 }
+
+
+// ignore NULL
+char* strpbrk(const char* s1, const char* s2)
+{
+    if (*s1 == 0)
+        return NULL;
+    
+    while (*s1) {
+        for (char* tmp = s2; *tmp != 0; ++tmp) {
+            if (*tmp == *s1)
+                return s1;
+        }
+        ++s1;
+    }
+    
+    return NULL;
+}
