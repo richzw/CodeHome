c6a9e2c8dfc13e54a94b3c93fb0bb02c22944b79
Wei Zang <richzw@gmail.com>
2014-10-23 21:21:22 +0800

    Update nk+b.py

--- Algorithm/NumberOccuring/nk+b.py
+++ Algorithm/NumberOccuring/nk+b.py
@@ -1,3 +1,5 @@
+#Ref: http://stackoverflow.com/questions/9442958/find-the-element-occurring-b-times-in-an-an-array-of-size-nkb?rq=1
+
 """
 Given an Array of size (n*k+b) where n elements occur k times and one element occurs b times, 
 in other words there are n+1 distinct Elements. Given that 0 < b < k find the element occurring b times.
@@ -35,3 +37,37 @@ def solve(data, k):
     return reduce(lambda a,b:2*a+(b%k>0), reversed(cnts), 0)
 
 print "Answer: ", solve(generate_test_data(10, 15, 13), 3)
+
+'''
+Method 2
+
+I assume:
+    The elements of the array are comparable.
+    We know the values of n and k beforehand.
+    A solution O(n*k+b) is good enough.
+
+Let the number occuring only b times be S. We are trying to find the S in an array of n*k+b size.
+
+Recursive Step: Find the median element of the current array slice as in Quick Sort in lineer time. 
+Let the median element be M.
+
+After the recursive step you have an array where all elements smaller than M occur on the left of the first occurence 
+of M. All M elements are next to each other and all element larger than M are on the right of all occurences of M.
+
+Look at the index of the leftmost M and calculate whether S<M or S>=M. Recurse either on the left slice or the right 
+slice.
+
+So you are doing a quick sort but delving only one part of the divisions at any time. You will recurse O(logN) times
+but each time with 1/2, 1/4, 1/8, .. sizes of the original array, so the total time will still be O(n).
+
+Clarification: Let's say n=20 and k = 10. Then, there are 21 distinct elements in the array, 
+20 of which occur 10 times and the last occur let's say 7 times. I find the medium element, 
+let's say it is 1111. If the S<1111 than the index of the leftmost occurence of 1111 will be less than 11*10. 
+If S>=1111 then the index will be equal to 11*10.
+
+Full example: n = 4. k = 3. Array = {1,2,3,4,5,1,2,3,4,5,1,2,3,5} After the first recursive step 
+I find the median element is 3 and the array is something like: {1,2,1,2,1,2,3,3,3,5,4,5,5,4} 
+There are 6 elements on the left of 3. 6 is a multiple of k=3. So each element must be occuring 3 times there. 
+So S>=3. Recurse on the right side. And so on.
+
+'''
