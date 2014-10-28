e7064c21e14b166d9e6cfde78bd6b0c4f1f7d165
Wei Zang <richzw@gmail.com>
2014-10-28 13:33:41 +0800

    Update threeNumsOnce.c

--- Algorithm/NumberOccuring/threeNumsOnce.c
+++ Algorithm/NumberOccuring/threeNumsOnce.c
@@ -1,10 +1,14 @@
-//Q: In a sequence of length n, where n=2k+3, that is there are k unique numbers appeared twice and three numbers appeared only once.
+//Q: In a sequence of length n, where n=2k+3, that is there are k unique numbers appeared twice and 
+//three numbers appeared only once.
 
 /*
 1)	If there is one number appeared only once, we can apply XOR to the sequence to find it.
-2)	If there are two, we can first apply XOR to the sequence, then separate the sequence into 2 parts by one bit of the result that is 1, and again apply XOR to the 2 parts, and we will find the answer.
+2)	If there are two, we can first apply XOR to the sequence, then separate the sequence into 2 parts
+        by one bit of the result that is 1, and again apply XOR to the 2 parts, and we will find the answer.
+        
 Method A: Bloom filter
-Create two Bloom filters. The first (A) contains numbers that have been found at least one, and the second (B) contains numbers that have been found twice.
+Create two Bloom filters. The first (A) contains numbers that have been found at least one, 
+and the second (B) contains numbers that have been found twice.
 A = empty
 B = empty
 
@@ -20,17 +24,19 @@ foreach x in the list
       print x
 
 method B: bit xor operation
-We need two integers for each bit of the numbers (e.g. 32 bits). For each number, if that bit is zero, XOR the first integer with it. otherwise, XOR the second integer with it.
-Also, keep count of how many times you find a 1 or 0 in each position (we only need to check if this is even or odd, so keep a boolean).
+We need two integers for each bit of the numbers (e.g. 32 bits). For each number, if that bit is zero, XOR the first integer
+with it. otherwise, XOR the second integer with it.
+Also, keep count of how many times you find a 1 or 0 in each position (we only need to check if this is even or odd,
+so keep a boolean).
 
-After iterating through, our pairs of integers will be one of the following. The first number here represents an even count, the second an odd.
+After iterating through, our pairs of integers will be one of the following. The first number here represents an even count, 
+the second an odd.
 0, a^b^c
 a^b, c
 a^c, b
 b^c, a
-
-For each pair, check the even count integer. If it is zero, then we know the other integer is a^b^c, since no two of our results will be equal.
-Otherwise, we've found a value at the odd count integer.
+For each pair, check the even count integer. If it is zero, then we know the other integer is a^b^c, 
+since no two of our results will be equal. Otherwise, we've found a value at the odd count integer.
 */
 void find3(int list[], int len) {
 	int xors[32][2] = {0};
