9a7188e5a6094c6a0b41ee3dc0d70900b6411653
Wei Zang <richzw@gmail.com>
2014-11-05 16:46:02 +0800

    Create Prime.c

---
+++ Maths/Prime.c
@@ -0,0 +1,25 @@
+// A function to print all prime factors of a given number n
+void primeFactors(int n)
+{
+    // Print the number of 2s that divide n
+    while (n%2 == 0)
+    {
+        printf("%d ", 2);
+        n = n/2;
+    }
+ 
+    // n must be odd at this point.  So we can skip one element (Note i = i +2)
+    for (int i = 3; i <= sqrt((double)n); i = i+2)
+    {
+        // While i divides n, print i and divide n
+        while (n%i == 0)
+        {
+            printf("%d ", i);
+            n = n/i;
+        }
+    }
+ 
+    // This condition is to handle the case whien n is a prime number greater than 2
+    if (n > 2)
+        printf ("%d ", n);
+}
