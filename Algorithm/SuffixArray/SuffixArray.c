412b2d9f89ac7b34dfa497a858182cb7df2fa3f4
Wei Zang <richzw@gmail.com>
2014-11-13 20:01:19 +0800

    Update SuffixArray.c

--- Algorithm/SuffixArray/SuffixArray.c
+++ Algorithm/SuffixArray/SuffixArray.c
@@ -1,3 +1,52 @@
 //http://www.geeksforgeeks.org/suffix-array-set-1-introduction/
 // build suffix array
 
+struct suffix{
+	int index;
+	char* suff;
+};
+
+int cmp(struct suffix a, struct suffix b){
+	return strcmp(a.suff, b.suff)< 0? 1:0;
+}
+
+int* buildSuffixArray(char* txt, int n) {
+	struct suffix suffixes[n];
+
+	// store suffixes and their indexes in an array of structure
+	for (int i = 0; i < n; i++){
+		suffixes[i].index = i;
+		suffixes[i].suff = txt+i;
+	}
+
+	// sort the suffixes 
+	sort(suffixes, suffixes+n, cmp);
+
+	int* suffixArr = new int[n];
+	for (int i = 0; i < n; ++i)
+		suffixArr[i] = suffixes[i].index;
+
+	return suffixArr;
+}
+
+void search(char* pat, char* txt, int* suffixArr, int n){
+	int m = strlen(pat);
+
+	int l = 0, r = n -1;
+	while (l <= r){
+		int mid = l + (r-l)/2;
+		int res = strncmp(pat, txt+suffixArr[mid], m);
+
+		if (res == 0){
+			cout << "Pattern found at index" << suffixArr[mid];
+			return;
+		}
+
+		if (res < 0)
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
+
+	cout << "Pattern Not Found";
+}
