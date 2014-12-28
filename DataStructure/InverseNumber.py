12341df5249a7310ba53ebcbbf645fd96ad3f262
zangw <richzw@gmail.com>
2014-12-28 09:33:09 +0800

    Create InverseNumber.py

---
+++ DataStructure/InverseNumber.py
@@ -0,0 +1,16 @@
+# BIT
+
+def count_inversions(a):
+  res = 0
+  counts = [0]*(len(a)+1)
+  rank = { v : i+1 for i, v in enumerate(sorted(a)) }
+  for x in reversed(a):
+    i = rank[x] - 1
+    while i:
+      res += counts[i]
+      i -= i & -i
+    i = rank[x]
+    while i <= len(a):
+      counts[i] += 1
+      i += i & -i
+  return res
