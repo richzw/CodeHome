4ffb5a17e1084fc95c22491e4025bfd2c2c31bab
zangw <richzw@gmail.com>
2015-01-08 16:22:38 +0800

    Create AStarDijk.py

---
+++ DataStructure/AStar/AStarDijk.py
@@ -0,0 +1,18 @@
+
+ def astar_dijk(graph, start, goal):
+	frontier = Queue.PriorityQueue()
+	frontier.put(start, 0)
+	came_from = {}
+	cost_so_far = {}
+	came_from[start] = None
+	cost_so_far[start] = 0
+	while not frontier.empty():
+		current = frontier.get()
+		if current == goal:
+			break
+		for neighbor in graph.neighbors(current):
+			new_cost = cost_so_far[current] + graph.cost(current, neighbor)
+			if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+				cost_so_far[neighbor] = new_cost
+				frontier.put(neighbor, new_cost)
+				came_from[neighbor] = current
