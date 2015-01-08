ebff1c848979efa82ba5436b2aef4f3054db3151
zangw <richzw@gmail.com>
2015-01-08 15:07:16 +0800

    Update AStar.py

--- DataStructure/AStar/AStar.py
+++ DataStructure/AStar/AStar.py
@@ -1,6 +1,6 @@
 '''
+simple version
 '''
-
 def floodfill(graph, start):
 	frontier = Queue()
 	frontier.put(start)
@@ -12,3 +12,34 @@ def floodfill(graph, start):
 			if neighbor not in visited:
 				frontier.put(neighbor)
 				visited[neighbor] = True
+
+'''
+Get path
+'''
+def floodfill(graph, start):
+	frontier = Queue()
+	frontier.put(start)
+	paths = {}
+	paths[start] = None
+	while not frontier.empty():
+		current = frontier.get()
+		for neighbor in graph.neighbors(current):
+			if neighbor not in paths:
+				frontier.put(neighbor)
+				paths[neighbor] = current
+				
+'''
+Get Distance
+'''
+def floodfill(graph, start):
+	frontier = Queue()
+	frontier.put(start)
+	distance = {}
+	distance[start] = 0
+	while not frontier.empty():
+		current = frontier.get()
+		for neighbor in graph.neighbors(current):
+			if neighbor not in distance:
+				frontier.put(neighbor)
+				distance[neighbor] = 1 + distance[current]
+				
