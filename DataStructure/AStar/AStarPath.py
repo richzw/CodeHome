327ca4ffce40c2ca4d5e68138d81e859ae57db5a
zangw <richzw@gmail.com>
2015-01-08 15:15:57 +0800

    Update AStarPath.py

--- DataStructure/AStar/AStarPath.py
+++ DataStructure/AStar/AStarPath.py
@@ -1,6 +1,20 @@
 """
 """
-# previous path solution
+frontier = Queue()
+frontier.put(start)
+came_from = {}
+came_from[start] = None
+
+while not frontier.empty():
+   current = frontier.get()
+
+   if current == goal:
+      break           
+
+   for next in graph.neighbors(current):
+      if next not in came_from:
+         frontier.put(next)
+         came_from[next] = current
 
 current = goal
 path = [current]
