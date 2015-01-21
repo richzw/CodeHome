3316c0065e3f942ebd8329af6074e81ecc6fcb2d
zangw <richzw@gmail.com>
2015-01-21 19:06:12 +0800

    Update AveragePartition.cpp

--- Algorithm/SubsetSum/AveragePartition.cpp
+++ Algorithm/SubsetSum/AveragePartition.cpp
@@ -27,3 +27,27 @@ bool AveragePartitionByRecur(vector<int> v){
 
 // Dynamic programming
 // part[i][j] = true if a subset of {arr[0], arr[1], ...arr[j-1]} has sum equal to i, otherwise false
+bool AveragePartitionDP(vector<int> v){
+	int n = v.size();
+	int sum = accumulate(v.begin(), v.end(), 0);
+
+	if (sum % 2 != 0)
+		return false;
+
+	vector<vector<bool> > dp(sum/2+1, vector<bool>(n+1));
+
+	// initialize the dp array
+	for (int idx = 0; idx < sum/2 + 1; ++idx)
+		dp[idx][0] = false;
+	for (int idx = 0; idx < n + 1; ++idx)
+		dp[0][idx] = true;
+	
+	for (int i = 1; i < sum/2+1; ++i)
+		for (int j = 1; j < n+1; ++j){
+			dp[i][j] = dp[i][j-1];
+			if (i >= v[j-1])
+				dp[i][j] = dp[i][j]|| dp[i-v[j-1]][j-1];
+		}
+
+	return dp[sum/2][n];
+}
