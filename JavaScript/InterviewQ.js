c0df3a0c9e3dd96f552d84fbf7e6d529f5393706
zangw <richzw@gmail.com>
2015-07-28 15:14:56 +0800

    Update InterviewQ.js

--- JavaScript/InterviewQ.js
+++ JavaScript/InterviewQ.js
@@ -10,6 +10,15 @@ console.log(b);
 })();
 console.log(b);
 
+// output??
+var data = [];
+for (var i = 0; i < 5; i++) {
+  data[i] = function foo() {
+    console.log(i);
+  };
+}
+data[0](); data[1](); data[2](); data[3](); data[4]();
+
 // add repeatify method to String like  console.log('hello'.repeatify(3));
 String.prototype.repeatify = String.prototype.repeatify || function(times) {
    var str = '';
