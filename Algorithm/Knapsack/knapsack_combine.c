214b6305f8104d7b5d198e70af1031f2253d6ac3
zangw <richzw@gmail.com>
2015-07-29 15:44:51 +0800

    Update knapsack_combine.c

--- Algorithm/Knapsack/knapsack_combine.c
+++ Algorithm/Knapsack/knapsack_combine.c
@@ -5,15 +5,15 @@ void find_path(int n, int w, int k);
 int pl[Num][Wt] = {0}; //0 not, 1 in, 2 in or not OK.
 void knapsack_combine(int n, int w){
   for (int i = n-1; i >= 0; --i)
-		for (int j = w; j >=weight[i]; --j)
-			if (c[j-weight[i]] + cost[i] > c[j]){
-				c[j] = c[j-weight[i]] + cost[i];
-				pl[i][j] = 1;
-			}else if (c[j-weight[i]] + cost[i] == c[j]){
-				pl[i][j] = 2;
-			}else if (c[j-weight[i]] + cost[i] < c[j]){
-				pl[i][j] = 0;
-			}
+	for (int j = w; j >=weight[i]; --j)
+		if (c[j-weight[i]] + cost[i] > c[j]){
+			c[j] = c[j-weight[i]] + cost[i];
+			pl[i][j] = 1;
+		}else if (c[j-weight[i]] + cost[i] == c[j]){
+			pl[i][j] = 2;
+		}else if (c[j-weight[i]] + cost[i] < c[j]){
+			pl[i][j] = 0;
+		}
 	int k = 0;
 	find_path(n-1, c[w], k);
 }
