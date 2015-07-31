8e643a245e18bc31009ea04252647dd51d3f7857
zangw <richzw@gmail.com>
2015-07-31 13:37:49 +0800

    Create MinWindowContainStr.cpp

---
+++ LeetCode/MinWindowContainStr.cpp
@@ -0,0 +1,15 @@
+/*
+Given a set T of characters and a string S, 
+find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+eg,
+S = “ADOBECODEBANC”
+T = “ABC”
+
+Minimum window is “BANC”.
+*/
+
+bool minWindow(const char* str, const char* pattern, int& minWinStart, int& minWinStop)
+{
+
+}
