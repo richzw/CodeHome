c5af790c3da391bd94dd78d3697e62674306175e
zangw <richzw@gmail.com>
2015-09-22 19:02:29 +0800

    Create BackTrack.cpp

---
+++ Algorithm/Permution/BackTrack.cpp
@@ -0,0 +1,22 @@
+bool finished = false;
+backtrack(int a[], int k, data input)
+{
+	int c[MAXCAND];
+	int ncand;
+	if (is_a_solution(a, k, input))
+		process_solution(a, k, input);
+	else 
+	{
+		k += 1;
+		construct_cand(a, k, input, c, &ncand);
+		for (int i = 0; i < ncand; ++i)
+		{
+			a[k] = c[i];
+			make_move(a, k, input);
+			backtrack(a, k, input);
+			unmake_move(a, k, input);
+			if (finished)
+				return;
+		}
+	}
+}
