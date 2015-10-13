195be526c41f096ea793e027f32f687c97ec2d7f
zangw <richzw@gmail.com>
2015-10-13 10:19:00 +0800

    Update sqrt.c

--- LeetCode/sqrt.c
+++ LeetCode/sqrt.c
@@ -13,6 +13,24 @@ int sqrt(int x) {
     return int(y);
 }
 
+double sqrt(double x) {
+    if (x <= 0)
+        return 0;       // if negative number throw an exception?
+    int exp = 0;
+    x = frexp(x, &exp); // extract binary exponent from x
+    if (exp & 1) {      // we want exponent to be even
+        exp--;
+        x *= 2;
+    }
+    double y = (1+x)/2; // first approximation
+    double z = 0;
+    while (y != z) {    // yes, we CAN compare doubles here!
+        z = y;
+        y = (y + x/y) / 2;
+    }
+    return ldexp(y, exp/2); // multiply answer by 2^(exp/2)
+}
+
 // binary search method 
 double sqaure_root_of(double value)
 {
