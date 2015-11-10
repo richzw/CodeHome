7171e9fa72189c46c0ef354f556aebf947a7ea2e
zangw <richzw@gmail.com>
2015-11-10 12:14:43 +0800

    Create Machine.cpp

---
+++ SoftwareDesign/StatePattern/Machine.cpp
@@ -0,0 +1,21 @@
+#include "Machine.h"
+
+Machine::Machine(int inStockQuantity) :
+    mStockQuantity(inStockQuantity), mState(new Normal()) {
+}
+
+Machine::~Machine() {
+    delete mState;
+}
+
+void Machine::sell(int quantity) {
+    mState->sell(*this, quantity);
+}
+
+void Machine::refill(int quantity) {
+    mState->refill(*this, quantity);
+}
+
+int Machine::getCurrentStock() {
+    return mStockQuantity;
+}
