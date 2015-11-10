b6f0de1d33261ee262376a6f673235c1696acb00
zangw <richzw@gmail.com>
2015-11-10 12:23:38 +0800

    Create test.cpp

---
+++ SoftwareDesign/StatePattern/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdexcept>
+#include "Machine.h"
+#include "MachineStates.h"
+
+int main() {
+    Machine m(10);
+
+    m.sell(10);
+    std::cout << "Sold 10 items" << std::endl;
+
+    try {
+        m.sell(1);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    m.refill(20);
+    std::cout << "Refilled 20 items" << std::endl;
+
+    m.sell(10);
+    std::cout << "Sold 10 items" << std::endl;
+    std::cout << "Remaining " << m.getCurrentStock() << " items" << std::endl;
+
+    m.sell(5);
+    std::cout << "Sold 5 items" << std::endl;
+    std::cout << "Remaining " << m.getCurrentStock() << " items" << std::endl;
+
+    try {
+        m.sell(10);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
