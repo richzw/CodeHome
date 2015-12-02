c8eefda848e3752283841891f695d8c0ac171c76
zangw <richzw@gmail.com>
2015-12-02 10:23:58 +0800

    Create StateMachine.h

---
+++ SoftwareDesign/StateMachine/StateMachine.h
@@ -0,0 +1,56 @@
+#ifndef STATE_MACHINE_H
+#define STATE_MACHINE_H
+#include <stdio.h>
+#include "EventData.h"
+
+struct StateStruct;
+
+// base class for state machines
+class StateMachine 
+{
+public:
+    StateMachine(int maxStates);
+    virtual ~StateMachine() {}
+protected:
+    enum { EVENT_IGNORED = 0xFE, CANNOT_HAPPEN };
+    unsigned char currentState;
+    void ExternalEvent(unsigned char, EventData* = NULL);
+    void InternalEvent(unsigned char, EventData* = NULL);
+    virtual const StateStruct* GetStateMap() = 0;
+private:
+    const int _maxStates;
+    bool _eventGenerated;
+    EventData* _pEventData;
+    void StateEngine(void);
+};
+
+typedef void (StateMachine::*StateFunc)(EventData *);
+struct StateStruct 
+{
+    StateFunc pStateFunc;    
+};
+
+#define BEGIN_STATE_MAP \
+public:\
+const StateStruct* GetStateMap() {\
+    static const StateStruct StateMap[] = { 
+
+#define STATE_MAP_ENTRY(entry)\
+    { reinterpret_cast<StateFunc>(entry) },
+
+#define END_STATE_MAP \
+    { reinterpret_cast<StateFunc>(NULL) }\
+    }; \
+    return &StateMap[0]; }
+
+#define BEGIN_TRANSITION_MAP \
+    static const unsigned char TRANSITIONS[] = {\
+
+#define TRANSITION_MAP_ENTRY(entry)\
+    entry,
+
+#define END_TRANSITION_MAP(data) \
+    0 };\
+    ExternalEvent(TRANSITIONS[currentState], data);
+
+#endif //STATE_MACHINE_H
