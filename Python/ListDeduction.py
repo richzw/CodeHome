793606216fac91d5ea54e2c407ab56ad06df0398
zangw <richzw@gmail.com>
2015-12-17 10:22:45 +0800

    Update ListDeduction.py

--- Python/ListDeduction.py
+++ Python/ListDeduction.py
@@ -28,3 +28,19 @@ for k in square_generator(0):
     print k
 #option 2
 g = list(square_generator(0))
+
+'''
+I have a list say l = [10,10,20,15,10,20]. I want to assign each unique value a certain "index" to get [1,1,2,3,1,2].
+'''
+>>> from itertools import count
+>>> from collections import defaultdict
+>>> lst = [10, 10, 20, 15, 10, 20]
+>>> d = defaultdict(count(1).next)
+>>> [d[k] for k in lst]
+[1, 1, 2, 3, 1, 2]
+
+'''
+The default_factory(i.e count(1).next in this case) passed to defaultdict is called only when Python encounters a missing key, 
+so for 10 the value is going to be 1, then for the next ten it is not a missing key anymore hence the previously calculated 1 is used, 
+now 20 is again a missing key and Python will call the default_factory again to get its value and so on.
+'''
