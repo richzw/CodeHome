e4fc83e0578ad2480d44d8fc6b88d302c97db16f
zangw <richzw@gmail.com>
2015-12-25 20:44:02 +0800

    Update FloatingNumber.js

--- JavaScript/FloatingNumber.js
+++ JavaScript/FloatingNumber.js
@@ -18,3 +18,15 @@ equal = (Math.abs(x - y) < 0.000001)
 
 (0.1 + 0.2).toFixed(10) == 0.3
 > true
+
+function add(num1, num2){
+  let r1, r2, m;
+  r1 = (''+num1).split('.')[1].length;
+  r2 = (''+num2).split('.')[1].length;
+
+  m = Math.pow(10,Math.max(r1,r2));
+  return (num1 * m + num2 * m) / m;
+}
+console.log(add(0.1,0.2));   //0.3
+console.log(add(0.15,0.2256)); //0.3756
+
