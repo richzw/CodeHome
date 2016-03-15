6b01f6e8276203928a011e858a95aebc29de987d
zangw <richzw@gmail.com>
2016-03-15 17:47:14 +0800

    Update GoodPart.py

--- Python/GoodPart.py
+++ Python/GoodPart.py
@@ -110,8 +110,29 @@ a.append(b())
 
 # attention: issue happened at multi-thread invironment!!!!!
 
+'''
+Consider I have a list of lists as:
 
+[[5, 10, 30, 24, 100], [1, 9, 25, 49, 81]]
+[[15, 10, 10, 16, 70], [10, 1, 25, 11, 19]]
+[[34, 20, 10, 10, 30], [9, 20, 25, 30, 80]]
 
+Now I want the sum of all indexes of first list's index wise and then the 2nd list  5+15+34=54   10+10+20=40 and so on as:
+
+[54,40,50, 50,200], [20,30,75,90,180]
+'''
+
+>>> data = [[[5, 10, 30, 24, 100], [1, 9, 25, 49, 81]],
+...         [[15, 10, 10, 16, 70], [10, 1, 25, 11, 19]],
+...         [[34, 20, 10, 10, 30], [9, 20, 25, 30, 80]]]
+>>> for res in zip(*data):
+...     print [sum(j) for j in zip(*res)] 
+... 
+[54, 40, 50, 50, 200]
+[20, 30, 75, 90, 180]
+
+>>> [[sum(item) for item in zip(*items)] for items in zip(*data)]
+[[54, 40, 50, 50, 200], [20, 30, 75, 90, 180]]
 
 
 
