1689fd00b66d48388d2f437c254f4fae437c4683
zangw <richzw@gmail.com>
2016-03-29 15:38:50 +0800

    Update InterleavingString.cpp

--- Algorithm/InterleavingString.cpp
+++ Algorithm/InterleavingString.cpp
@@ -89,3 +89,63 @@ bool isInterleaving_v2(string s1, string s2, string s3){
     }
     return isPrefix.back();
 }
+
+/*
+If the two strings are 'ab' and 'cd', the output I wish to get is:
+
+['abcd', 'acbd', 'acdb', 'cabd', 'cadb', 'cdab']
+*/
+
+void printIlsRecur (char *str1, char *str2, char *iStr, int m, 
+                    int n, int i)
+{
+    // Base case: If all characters of str1 and str2 have been 
+    // included in output string, then print the output string
+    if (m==0 && n==0)
+        printf("%s\n", iStr) ;
+ 
+    // If some characters of str1 are left to be included, then
+    // include the  first character from the remaining characters
+    // and recur for rest
+    if (m != 0)
+    {
+        iStr[i] = str1[0];
+        printIlsRecur (str1 + 1, str2, iStr, m-1, n, i+1);
+    }
+ 
+    // If some characters of str2 are left to be included, then
+    // include the  first character from the remaining characters 
+    // and recur for rest
+    if (n != 0)
+    {
+        iStr[i] = str2[0];
+        printIlsRecur(str1, str2+1, iStr, m, n-1, i+1);
+    }
+}
+ 
+// Allocates memory for output string and uses printIlsRecur()
+// for printing all interleavings
+void printIls (char *str1, char *str2, int m, int n)
+{
+   // allocate memory for the output string
+   char *iStr= (char*)malloc((m+n+1)*sizeof(char));
+ 
+   // Set the terminator for the output string
+   iStr[m+n] = '\0';
+ 
+   // print all interleavings using printIlsRecur()
+   printIlsRecur (str1, str2, iStr, m, n, 0);
+ 
+   // free memory to avoid memory leak
+   free(iStr); 
+}
+
+// python version
+def interleave(s, t, res, i, j, lis):
+    if i == len(s) and j == len(t):
+        lis.append(res)
+        return
+    if i < len(s):
+        interleave(s, t, res + s[i], i + 1, j, lis)
+    if j < len(t):
+        interleave(s, t, res + t[j], i, j + 1, lis)
