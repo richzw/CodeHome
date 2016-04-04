12b641097680b4b671e175c1442eeb271f9d1c81
zangw <richzw@gmail.com>
2016-04-04 22:26:34 +0800

    Update Introduction.js

--- JavaScript/Functional/Introduction.js
+++ JavaScript/Functional/Introduction.js
@@ -24,3 +24,31 @@ function pmTailReverse(list) {
     })(list, []);
 }
 
+// http://jimliu.net/2015/10/14/curry-function-in-es6/
+// curry 
+var curry = fn => (...left) => (...right) => fn(...left, ...right)
+var concat = (...args) => args.join('')
+
+var hehe = curry(concat)('hehe', 'haha')
+console.log(hehe('heihei')) // hehehahaheihei
+
+var curryR = fn => (...right) => (...left) => fn(...left, ...right)
+
+Function.prototype.curry = function(...left) {
+  return curry(this)(...left)
+}
+
+Function.prototype.curryR = function(...right) {
+  return curryR(this)(...right)
+}
+
+var append = concat.curry('hehe')
+var prepend = concat.curryR('hehe')
+
+console.log(append('1')) // hehe1
+console.log(prepend('1')) // 1hehe
+
+var saySomethingTo = (word, name, end) => concat(word, ', ', name, end)
+
+var sayHelloTo = saySomethingTo.curry('Hello').curryR('!!!')
+console.log(sayHelloTo('Jim')) 
