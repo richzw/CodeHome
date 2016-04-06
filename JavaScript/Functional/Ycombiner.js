9295c256d3d7dedc7448b3fe15e1219a8ea11fc8
zangw <richzw@gmail.com>
2016-04-06 20:17:13 +0800

    Update Ycombiner.js

--- JavaScript/Functional/Ycombiner.js
+++ JavaScript/Functional/Ycombiner.js
@@ -8,4 +8,28 @@ const Y = f =>
     (x => f(y => x(x)(y)))
     (x => f(y => x(x)(y)))
 
+// sampel codes
+const factorial = Y(
+    f => n => n == 0 ? 1 : n * f(n - 1)
+)
 
+const reverse = Y(
+   rev => ([x, ...xs]) =>
+       x === undefined ?
+       [] :
+       [...rev(xs), x]
+)
+
+const tailReverse = seq => Y(
+    rev => ([x, ...xs]) => ret =>
+        x === undefined ?
+        ret :
+        rev(xs)([x, ...ret])
+)(seq)([])
+
+const gcd = (a, b) => Y(
+    _gcd => a => b =>
+        b == 0 ? a : _gcd(b)(a % b)
+)(a)(b)
+
+// http://jimliu.net/2015/10/15/functional-reverse-in-es6/
