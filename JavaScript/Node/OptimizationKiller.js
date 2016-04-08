7647a177e5e0128549f9126677613b670019251c
zangw <richzw@gmail.com>
2016-04-08 15:12:22 +0800

    Update OptimizationKiller.js

--- JavaScript/Node/OptimizationKiller.js
+++ JavaScript/Node/OptimizationKiller.js
@@ -1,6 +1,6 @@
 // Source: https://github.com/petkaantonov/bluebird/wiki/Optimization-killers
 
-/*
+/********************************************************************************
  * Tools
 */
 //Function that contains the pattern to be inspected (using with statement)
@@ -35,6 +35,41 @@ printStatus(containsWith);
 
 // Run the above codes
 $ node --trace_opt --trace_deopt --allow-natives-syntax test.js
+
+/********************************************************************************
+ * Unsurpported syntex 
+*/
+
+// workaround to avoid `try-catch` in production codes
+
+var errorObject = {value: null};
+function tryCatch(fn, ctx, args) {
+    try {
+        return fn.apply(ctx, args);
+    }
+    catch(e) {
+        errorObject.value = e;
+        return errorObject;
+    }
+}
+
+var result = tryCatch(mightThrow, void 0, [1,2,3]);
+//Unambiguously tells whether the call threw
+if(result === errorObject) {
+    var error = errorObject.value;
+}
+else {
+    //result is the returned value
+}
+
+/**
+ * safe arguments usage
+ * 
+ * Only use
+ *   - arguments.length
+ *   - arguments[i] where i is always a valid integer index into the arguments, and cannot be out of bound
+ *   - STRICTLY fn.apply(y, arguments)
+*/
 
 
 
