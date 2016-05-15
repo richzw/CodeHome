0741a9468fc522ec939456ce706a56ac56b67725
zangw <richzw@gmail.com>
2016-05-15 13:46:27 +0800

    Update 2DArray.cpp

--- DataStructure/2DArray.cpp
+++ DataStructure/2DArray.cpp
@@ -1,7 +1,10 @@
 // Two dimensional array
 
-	int** ary = new int*[row];
-	for (int r = 0; r < row; ++r)
-		ary[r] = new int[column];
+int** ary = new int*[row];
+for (int r = 0; r < row; ++r)
+	ary[r] = new int[column];
 
-	vector<vector<int> > dyAry(row, vector<int>(column));
+vector<vector<int> > dyAry(row, vector<int>(column));
+	
+// another method to create 2-dim array
+double (*eggFloor)[e+1] = (int(*)[e+1])malloc((f+1) * sizeof(*eggFloor));
