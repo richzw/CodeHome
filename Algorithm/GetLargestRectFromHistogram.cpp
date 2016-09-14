29073746d7d58ea3790507769b4aefb8bb3b022b
zangw <richzw@gmail.com>
2016-09-14 13:57:29 +0800

    Update GetLargestRectFromHistogram.cpp

--- Algorithm/GetLargestRectFromHistogram.cpp
+++ Algorithm/GetLargestRectFromHistogram.cpp
@@ -42,3 +42,78 @@ int get_largest_rect(vector<int> vec){
 
 	return max_area;
 }
+
+// RMQ solution
+int maxVal(int x, int y, int z) {
+	return max( max(x, y), z);
+}
+
+int minVal(int* hist, int i, int j) {
+	// check the range of i and j
+	if (i == -1) return j;
+	if (j == -1) return i;
+	return (hist[i] < hist[j]) ? i : j;
+}
+
+int midVal(int s, int e) {
+	return s + (e - s) / 2;
+}
+
+int constructSTUtil(int hist[], int ss, int se, int* st, int si) {
+	if (ss == se)
+		return st[si] = ss;
+
+	int mid = midVal(ss, se);
+	st[si] = minVal(hist, constructSTUtil(hist, ss, mid, st, si*2 + 1),
+				constructSTUtil(hist, mid+1, se, st, si*2 + 2));
+
+	return st[si];
+}
+
+int* constructST(int hist[], int n) {
+	int hi = (int)ceil(log2(n));
+	int max_size = 2 * (int)pow(2, hi) - 1;
+	int* st = new int[max_size];
+
+	constructSTUtil(hist, 0, n - 1, st, 0);
+
+	return st;
+}
+
+int RMQUtil(int* hist, int* st, int ss, int se, int qs, int qe, int idx) {
+	if (qs <= ss && se <= qe)
+		return st[idx];
+
+	if (se < qs || ss > qe)
+		return -1;
+
+	int mid = midVal(ss, se);
+	return minVal(hist, RMQUtil(hist, st, ss, mid, qs, qe, 2*idx + 1),
+			    RMQUtil(hist, st, mid+1, se, qs, qe, 2*idx + 2));
+}
+
+
+int RMQ(int* hist, int* st, int n, int qs, int qe) {
+	if (qs < 0 || qe > n - 1 || qs > qe)
+		return -1;
+
+	return RMQUtil(hist, st, 0, n-1, qs, qe, 0);
+}
+
+int getMaxAreaRect(int* hist, int* st, int n, int l, int r) {
+	if (l > r) return INT_MIN;
+	if (l == r) return hist[l];
+
+	int m = RMQ(hist, st, n, l, r);
+
+	return maxVal(getMaxAreaRect(hist, st, n, l, m- 1),
+			getMaxAreaRect(hist, st, n, m + 1, r),
+			(r-l+1)*hist[m]);
+}
+
+int getMaxArea(int hist[], int n) {
+	int* st = constructST(hist, n);
+
+	return getMaxAreaRect(hist, st, n, 0, n - 1);
+}
+
