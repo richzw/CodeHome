d1b577d71768b46523c3c5b7b1f11007a9156d5f
zangw <richzw@gmail.com>
2016-09-21 11:23:39 +0800

    Update UnioinByHeight.c

--- Algorithm/UnionFind/UnioinByHeight.c
+++ Algorithm/UnionFind/UnioinByHeight.c
@@ -4,3 +4,31 @@
  then rank is not always equal to height. Also, size (in place of height) of trees can also be used as rank.
  
 */
+
+const int UFARRLEN = 10;
+int UFArr[UFARRLEN] = {0};
+
+void UF_init() {
+	for (int i = 0; i < UFARRLEN; ++i)
+		UFArr[i] = i;
+}
+
+int UF_root(int i) {
+	while (i != UFArr[i]) {
+		UFArr[i] = UFArr[UFArr[i]]; // one line for path compression
+		i = UFArr[i];
+	}
+
+	return i;
+}
+
+bool UF_find(int i, int j) {
+	return UF_root(i) == UF_root(j);
+}
+
+void UF_union(int i, int j) {
+	int ri = UF_root(i);
+	int rj = UF_root(j);
+
+	UFArr[ri] = rj;
+}
