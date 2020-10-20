0c17b20549788f59323202c27b963adbd1ac5cf2
臧伟 <richzw@gmail.com>
2020-10-20 09:26:14 +0800

    Update SpinLock.go

--- Go/SpinLock.go
+++ Go/SpinLock.go
@@ -1,15 +1,18 @@
+
+
 type spinLock uint32
-
 func (sl *spinLock) Lock() {
-    for !atomic.CompareAndSwapUint32((*uint32)(sl), 0, 1) {
-        runtime.Gosched() //without this it locks up on GOMAXPROCS > 1
-    }
+	for !atomic.CompareAndSwapUint32((*uint32)(sl), 0, 1) {
+		runtime.Gosched() //without this it locks up on GOMAXPROCS > 1
+	}
 }
-
 func (sl *spinLock) Unlock() {
-    atomic.StoreUint32((*uint32)(sl), 0)
+	atomic.StoreUint32((*uint32)(sl), 0)
 }
-
+func (sl *spinLock) TryLock() bool {
+	return atomic.CompareAndSwapUint32((*uint32)(sl), 0, 1)
+}
 func SpinLock() sync.Locker {
-    return &spinLock{}
+	var lock spinLock
+	return &lock
 }
